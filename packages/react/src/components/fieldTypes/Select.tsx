ecb7198ab68d57f800532d71344b44c7fa33a6dd
sagar <spandey@mblm.com>
2022-11-15 16:51:26 +0545

    change in select

--- packages/react/src/components/fieldTypes/Select.tsx
+++ packages/react/src/components/fieldTypes/Select.tsx
@@ -85,7 +85,6 @@ const FormSelecSelect: React.FC<Props> = ({
   }
 
   const handleSelectChange = (value: SingleValue<KeyValue>) => {
-    setCurrValue(Object.values(preValues).indexOf(value))
      if (typeof preValues === 'object' && Array.isArray(preValues) === false) {
        setCurrValue(Object.values(preValues).indexOf(value))
      } else setCurrValue(value)
